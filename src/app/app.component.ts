57cfb37cd67533ecccc482fc639c79b29124ec6c
Juan <plascencia.juan831@gmail.com>
2017-10-12 14:19:30 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  dojomail: any[] = [{ email: "bil@gates.com", importance: true, subject: "New Windows", content: "Windows XI will launch in year 2010"},
+  { email: "ada@lovelace.com", importance: true, subject: "Programming", content: "Enchantress of Numbers "},
+  { email: "john@carmac.com", importance: false, subject: "Updated Algo", content: "New Algorithm for shadow volumes."},
+  { email: "gabe@newel.com", importance: false, subject: "HL3!", content: "Just Kidding..."}];
 }
